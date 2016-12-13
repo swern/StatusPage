8e2fd01a86af4199f9a5f73166c5a6312d89df24
Sam Werngren <sam.werngren@gmail.com>
2016-12-13 14:44:55 +0000

    fleshed out models

---
+++ client/src/models/event.js
@@ -0,0 +1,22 @@
+var Event = function(title, status, content, schedule, lastUpdated, comments){
+
+	this.title = title;
+	this.status = status;
+	this.content = content;
+	this.schedule = schedule;
+	this.lastUpdated = lastUpdated;
+	this.comments = comments;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+module.exports = Event;
